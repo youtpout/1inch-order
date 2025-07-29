b11ead963fed701b3bccf43342ef0698a6ecca58
eddy <eddyboughioul@hotmail.fr>
2025-07-29 13:07:02 +0200

    copy and implement preinteraction

--- contracts/contracts/PositionOrder.sol
+++ contracts/contracts/PositionOrder.sol
@@ -5,20 +5,29 @@ import {IERC721} from "./interfaces/IERC721.sol";
 import {INonfungiblePositionManager} from "./interfaces/INonfungiblePositionManager.sol";
 import {AggregatorV3Interface} from "./interfaces/AggregatorV3Interface.sol";
 import {ERC721Proxy} from "./ERC721Proxy.sol";
+import {IPreInteraction} from "./interfaces/IPreInteraction.sol";
 
 /// @notice Contract designed to sell Uniswap v3 compatible ERC721 Position
 /// Based on 1inch proxy ERC721 proxy
-contract PositionOrder is ERC721Proxy {
-    constructor(
-        address _immutableOwner
-    ) ERC721Proxy(_immutableOwner) {
-    }
+contract PositionOrder is ERC721Proxy, IPreInteraction {
+    constructor(address _immutableOwner) ERC721Proxy(_immutableOwner) {}
 
-    /// @notice Post interaction to collect maker fees on position
-    function collectFees(
-        address _positionManager,
-        uint256 _tokenId
+    /// @notice Pre interaction to collect maker fees on position
+    function preInteraction(
+        Order calldata order,
+        bytes calldata extension,
+        bytes32 orderHash,
+        address taker,
+        uint256 makingAmount,
+        uint256 takingAmount,
+        uint256 remainingMakingAmount,
+        bytes calldata extraData
     ) external onlyImmutableOwner {
+        (address _positionManager, uint256 _tokenId) = abi.decode(
+            extraData,
+            (address, uint256)
+        );
+
         INonfungiblePositionManager positionManager = INonfungiblePositionManager(
                 _positionManager
             );
@@ -34,6 +43,8 @@ contract PositionOrder is ERC721Proxy {
             );
         positionManager.collect(collectParams);
     }
+
+   
 
     /// @notice Predicate function to get asset price to easily create stop loss order
     /// @dev Can be used via predicate builder as a call to this contract
